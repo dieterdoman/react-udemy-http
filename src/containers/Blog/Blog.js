e2420eb543e9785580ce31a862d2481bda486679
Dieter Doman <dieter@jemstep.com>
2019-08-02 11:55:40 +0200

    Added functionality to select a post.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import axios from 'axios';
 
 class Blog extends Component {
     state = {
-      posts: []
+      posts: [],
+      selectedPostId: null
     };
 
     componentDidMount() {
@@ -22,11 +23,15 @@ class Blog extends Component {
             });
             this.setState({posts: updatedPosts});
         });
-    }
+    };
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    };
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/>
         });
         return (
             <div>
@@ -34,7 +39,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
