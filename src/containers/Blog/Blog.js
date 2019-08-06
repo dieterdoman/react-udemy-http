61f5e4c68b7c998dea6325e893f4e98a2b2186cd
Dieter Doman <dieter@jemstep.com>
2019-08-06 11:50:40 +0200

    Using link component instead of a tag to prevent unneccassary page reloads.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import './Blog.css';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
     render () {
@@ -10,12 +11,13 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
