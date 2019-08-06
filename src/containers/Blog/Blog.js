d2ee74ad054cd014a3e7712c185fc87d547d27b1
Dieter Doman <dieter@jemstep.com>
2019-08-06 11:43:59 +0200

    Render component in route.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import './Blog.css';
+import {Route} from 'react-router-dom';
 
 class Blog extends Component {
     render () {
@@ -14,7 +15,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                <Route path="/" exact component={Posts}/>
             </div>
         );
     }
